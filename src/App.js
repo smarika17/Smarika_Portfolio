18558b17141a990dbe531e5f080010beafa8abc1
smarika17 <malviyasmarika@gmail.com>
2024-01-05 18:02:23 +0530

    TabCode Commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-     <NavBar />
-     <Banner />
-     <Skills />
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
     </div>
   );
 }
